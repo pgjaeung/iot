60bf03dcd5be067685671ebdd228c30babdc0a4a
jinsook <jinsook64@naver.com>
2019-12-07 19:41:21 +0900

    first commit

---
+++ week5/week5.ino
@@ -0,0 +1,12 @@
+int ledPin = 7; // ledPin을 7번으로 정의
+
+void setup() {
+  pinMode(ledPin, OUTPUT);    //ledPin을 출력으로 설정
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); //LED ON
+  delay(1000);                //1초 대기
+  digitalWrite(ledPin, LOW);  //LED OFF
+  delay(1000);                //1초 대기
+}
